954d018c576068b14bc73a26a8931acccaff9ea4
Daryatnt <90785640+Daryatnt@users.noreply.github.com>
2022-02-09 16:35:48 +0330

    Add files via upload

---
+++ scripts/pub.py
@@ -0,0 +1,45 @@
+from logging import shutdown
+from multiprocessing import shared_memory
+from multiprocessing.managers import SharedMemoryManager
+from re import A
+import string
+import cv2
+from cv2 import waitKey
+import numpy as np
+import rospy
+from std_msgs.msg import Int16MultiArray,String
+if __name__ == "__main__":
+    rospy.init_node('publisher', anonymous=True)
+    pub = rospy.Publisher("trying",String, queue_size=10)
+    #a = np.array([1, 1, 2, 3, 5, 8])  # Start with an existing NumPy array
+    
+   #  ret,frame=cap.read()
+   #  cv2.imshow('cam',frame)
+   #  shm = shared_memory.SharedMemory(create=True, size=a.nbytes)
+    
+ # Now create a NumPy array backed by shared memory
+   #  b = np.ndarray(a.shape, dtype=a.dtype, buffer=shm.buf)
+   #  b[:] = a[:]  # Copy the original data into shared memory
+ 
+   #  shm.close() 
+   #  shm.unlink() 
+   #  rospy.spin()
+
+while not rospy.is_shutdown():
+   cap=cv2.VideoCapture(0)
+   while(True):
+      ret,frame=cap.read()
+      a=frame
+      cv2.imshow('cam',frame) 
+      shm = shared_memory.SharedMemory(create=True, size=a.nbytes)
+      b = np.ndarray(a.shape, dtype=a.dtype, buffer=shm.buf)
+      b[:] = a[:]  # Copy the original data into shared memory
+      pub.publish(str(shm.name))
+      if cv2.waitKey(1)==ord('q'):
+            break
+    
+      
+
+
+
+
---
+++ scripts/sub.py
@@ -0,0 +1,33 @@
+from multiprocessing import shared_memory
+from multiprocessing.managers import SharedMemoryManager
+import string
+from tkinter import N
+from unicodedata import name
+from cv2 import waitKey
+import numpy as np
+import rospy
+from std_msgs.msg import String,Int16MultiArray
+import cv2
+
+def callback(na):
+    global N
+    N=na.data
+    
+    existing_shm = shared_memory.SharedMemory(name=str(N))
+    # Note that a.shape is (6,) and a.dtype is np.int64 in this example
+    c = np.ndarray((6,), dtype=np.int64, buffer=existing_shm.buf)
+    print(c)
+
+###########################PROBLEM IS HERE
+    # cv2.imshow('recieved',c)
+    # waitKey(1)
+
+
+    # existing_shm.close()
+    # existing_shm.unlink()
+    
+if __name__ == "__main__":
+    rospy.init_node('subscriber', anonymous=True)
+    rospy.Subscriber("trying",String, callback)
+    
+    rospy.spin()
